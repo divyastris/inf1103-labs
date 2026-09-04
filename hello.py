132e9fcd1a2c666ee68763b64490a44b1e6a9826
divya <2603540@sit.singaporetech.edu.sg>
2026-09-04 17:24:03 +0800

    Update Profile

--- hello.py
+++ hello.py
@@ -3,3 +3,12 @@ print("================================")
 print("Welcome here")
 print("My first post|")
 print("================================")
+
+#Activity 2: Update your Profile and add it to the repository
+username = "cool_creator"
+bio = "Fun Blogger"
+followers = 100
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
