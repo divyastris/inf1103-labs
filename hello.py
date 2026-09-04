9ef7d81b8641721ac378eab9f105ecd91f85ef42
Divya <2603540@sit.singaporetech.edu.sg>
2026-09-04 17:00:13 +0800

    changes

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
+#Activity 1: Create your First Python Program and add it to the repository
 print("================================")
 print("Welcome here")
 print("My first post|")
 print("================================")
 
+#Activity 2: Update your Profile and add it to the repository
 username = "cool_creator"
 bio = "Fun Blogger"
 followers = 100
@@ -11,6 +13,7 @@ print("Username:", username)
 print("Bio:", bio)
 print("Followers:", followers)
 
+#Activity 3: Follower Growth Tracker
 followers = 100
 
 followers += 50
@@ -21,3 +24,14 @@ print("Day 2:", followers)
 
 followers -= 10
 print("Day 3:", followers)
+
+#Activity 4: Interactive profile creator
+username= input("Enter Username: ")
+age = input("Enter Age: ")
+category = input("Enter Content Category: ")
+
+print("\nInstagram Profile")
+print("====================")
+print("Username:", username)
+print("Age:", age)
+print("Category:", category)
