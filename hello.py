8a48f1538d13dbab6a0589ff3807fa0ecd0b7f9b
divya <2603540@sit.singaporetech.edu.sg>
2026-09-04 17:44:11 +0800

    Changes

--- hello.py
+++ hello.py
@@ -35,3 +35,17 @@ print("====================")
 print("Username:", username)
 print("Age:", age)
 print("Category:", category)
+
+#Activity 5: Extra Things
+username - input("Enter USername: ")
+age = int(input("Enter Age: "))
+category = input("Enter Content Category: ")
+
+print("\nInstagram Profile")
+print("====================")
+print("Username:", username)
+print("Age:", age)
+print("Category:", category)
+
+if age>40 and category == "fun":
+    print("You are old what is fun for you??")
