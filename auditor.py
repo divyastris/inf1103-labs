83ceacd0a08840e7e621e2d00c0afbd1a68414ba
divya <2603540@sit.singaporetech.edu.sg>
2026-09-11 17:52:47 +0800

    Add invalid input validation

--- auditor.py
+++ auditor.py
@@ -7,3 +7,9 @@ while True:
     if stock.lower() == "quit":
         break
 
+    if not stock.isdigit():
+        print("Invalid input. Please enter a whole number.")
+        failed_entries += 1
+        continue
+
+    stock = int(stock)
